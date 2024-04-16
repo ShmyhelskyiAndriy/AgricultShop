4e8f3f10c19814a1eaffb94c651b2a3c21386110
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2024-04-16 15:31:50 +0300

    0.4 beta

---
+++ src/test/java/com/example/DemoTestBot/DemoTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoTestBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
