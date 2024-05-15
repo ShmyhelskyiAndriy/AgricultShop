eedc67341cf02ccb4dce92350774bac48427cbdd
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2024-05-16 01:51:52 +0300

    0.9v

--- src/main/java/com/example/DemoTestBot/service/TechSupportBot.java
+++ src/main/java/com/example/DemoTestBot/service/TechSupportBot.java
@@ -1,1 +1,66 @@
-package com.example.DemoTestBot.service;import org.springframework.stereotype.Component;import org.telegram.telegrambots.bots.TelegramLongPollingBot;import org.telegram.telegrambots.meta.api.methods.send.SendMessage;import org.telegram.telegrambots.meta.api.objects.Update;import org.telegram.telegrambots.meta.exceptions.TelegramApiException;@Componentpublic class TechSupportBot extends TelegramLongPollingBot {    @Override    public String getBotUsername() {        return "@AgriShop_bot";    }    @Override    public String getBotToken() {        return "6557682989:AAHswNBrja5vTGNxUt76kFES_a7cuTVk5sg";    }    private final long adminChatID = 795973878;    private long userChatID;    @Override    public void onUpdateReceived(Update update) {        if (update.hasMessage() && update.getMessage().hasText()) {            if(update.getMessage().getChatId()==adminChatID){                String reply = update.getMessage().getText();                userChatID = Long.parseLong(reply.split("\n")[0]);                SendMessage sendMessage = new SendMessage();                sendMessage.setChatId(userChatID);                sendMessage.setText("Дякуємо, за ваше звернення, ми розглянемо його та зв'яжимося з вами, якщо буде потреба");                try{                    execute(sendMessage);                }catch (TelegramApiException e){                    e.printStackTrace();                }                reply  = reply.replace(reply.split("\n")[0], " ");                sendMessage.setChatId(userChatID);                sendMessage.setText("Надійшла відповідь: " + reply + "\nВід модератора: " + update.getMessage().getFrom().getFirstName());                try{                    execute(sendMessage);                }catch (TelegramApiException e){                    e.printStackTrace();                }            }            else {                userChatID=update.getMessage().getChatId();                String request = update.getMessage().getText();                SendMessage sendRequest = new SendMessage();                sendRequest.setChatId(adminChatID);                sendRequest.setText("Username: " + update.getMessage().getFrom().getUserName() +"\nChatID: "+ userChatID +"\n "+ request);                try {                    execute(sendRequest);                }catch (TelegramApiException e){                    e.printStackTrace();                }            }        }    }}
+package com.example.DemoTestBot.service;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TechSupportBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "@AgriShop_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6557682989:AAHswNBrja5vTGNxUt76kFES_a7cuTVk5sg";
+    }
+
+    private final long adminChatID = 795973878;
+    private long userChatID;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            if(update.getMessage().getChatId()==adminChatID){
+                String reply = update.getMessage().getText();
+                userChatID = Long.parseLong(reply.split("\n")[0]);
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(userChatID);
+                sendMessage.setText("Дякуємо, за ваше звернення, ми розглянемо його та зв'яжимося з вами, якщо буде потреба");
+                try{
+                    execute(sendMessage);
+                }catch (TelegramApiException e){
+                    e.printStackTrace();
+                }
+
+
+                reply  = reply.replace(reply.split("\n")[0], " ");
+                sendMessage.setChatId(userChatID);
+                sendMessage.setText("Надійшла відповідь: " + reply + "\nВід модератора: " + update.getMessage().getFrom().getFirstName());
+                try{
+                    execute(sendMessage);
+                }catch (TelegramApiException e){
+                    e.printStackTrace();
+                }
+            }
+            else {
+                userChatID=update.getMessage().getChatId();
+                String request = update.getMessage().getText();
+
+                SendMessage sendRequest = new SendMessage();
+                sendRequest.setChatId(adminChatID);
+                sendRequest.setText("Username: " + update.getMessage().getFrom().getUserName() +"\nChatID: "+ userChatID +"\n "+ request);
+                try {
+                    execute(sendRequest);
+                }catch (TelegramApiException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
